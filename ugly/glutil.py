3ecc97495f9cd04b8f1006f7ed17f559ab7a1b7f
Johan Forsberg <johan@slentrian.org>
2019-11-03 01:39:56 +0100

    Move som utils around.

--- ugly/glutil.py
+++ ugly/glutil.py
@@ -1,11 +1,10 @@
 from ctypes import byref
 from functools import lru_cache
-from time import time
 
-import pyglet
+from pyglet import gl
 
 
-_gl_matrix = pyglet.gl.GLfloat * 16
+_gl_matrix = gl.GLfloat * 16
 
 
 @lru_cache(256)
@@ -14,53 +13,7 @@ def gl_matrix(mat):
 
 
 def get_max_texture_size():
-    max_texture_size = pyglet.gl.GLint()
-    pyglet.gl.glGetIntegerv(pyglet.gl.GL_MAX_TEXTURE_SIZE, byref(max_texture_size))
+    max_texture_size = gl.GLint()
+    gl.glGetIntegerv(gl.GL_MAX_TEXTURE_SIZE, byref(max_texture_size))
     return max_texture_size.value
-
-
-def throttle(interval=0.1):
-    """
-    A decorator that ensures that the function is not run more often
-    than the given interval, no matter how often it's called.
-    Uses the pyglet clock to schedule calls.
-    """
-    scheduled_at = None
-
-    def wrap(f):
-        def inner(*args, **kwargs):
-            nonlocal scheduled_at
-            now = time()
-            if scheduled_at and scheduled_at > now:
-                return
-            scheduled_at = now + interval
-
-            pyglet.clock.schedule_once(lambda dt: f(*args, **kwargs), interval)
-        return inner
-
-    return wrap
-
-
-def debounce(interval=0.1):
-    """
-    Decorator that causes the function to be delayed until such time that it has
-    not been called for at least interval seconds. Useful for preventing a function from
-    being run several times in rapid succession.
-    """
-    last_func = None
-
-    def wrap(f):
-        def inner(*args, **kwargs):
-            nonlocal last_func
-            if last_func:
-                pyglet.clock.unschedule(last_func)
-            last_func = lambda dt: f(*args, **kwargs)
-            pyglet.clock.schedule_once(last_func, interval)
-        return inner
-
-    return wrap
-
-
-
-immediately = throttle(interval=0)  # convenient for calling GL stuff from another thread
 
--- ugly/util.py
+++ ugly/util.py
@@ -1,4 +1,9 @@
+from contextlib import contextmanager
 import logging
+from time import time
+
+import pyglet
+from pyglet import gl
 
 
 class LoggerMixin:
@@ -18,3 +23,73 @@ def try_except_log(f):
         except Exception:
             logging.exception(f"Exception caught in callback {f}.")
     return inner
+
+
+def throttle(interval=0.1):
+    """
+    A decorator that ensures that the function is not run more often
+    than the given interval, no matter how often it's called.
+    Uses the pyglet clock to schedule calls.
+    """
+    scheduled_at = None
+
+    def wrap(f):
+        def inner(*args, **kwargs):
+            nonlocal scheduled_at
+            now = time()
+            if scheduled_at and scheduled_at > now:
+                return
+            scheduled_at = now + interval
+
+            pyglet.clock.schedule_once(lambda dt: f(*args, **kwargs), interval)
+        return inner
+
+    return wrap
+
+
+def debounce(interval=0.1):
+    """
+    Decorator that causes the function to be delayed until such time that it has
+    not been called for at least interval seconds. Useful for preventing a function from
+    being run several times in rapid succession.
+    Uses the pyglet clock to schedule calls.
+    """
+    last_func = None
+
+    def wrap(f):
+        def inner(*args, **kwargs):
+            nonlocal last_func
+            if last_func:
+                pyglet.clock.unschedule(last_func)
+            last_func = lambda dt: f(*args, **kwargs)
+            pyglet.clock.schedule_once(last_func, interval)
+        return inner
+
+    return wrap
+
+
+immediately = throttle(interval=0)  # convenient for calling GL stuff from another thread
+
+
+@contextmanager
+def enabled(*gl_flags):
+    enabled = set()
+    for f in gl_flags:
+        if not gl.glIsEnabled(f):
+            enabled.add(f)
+            gl.glEnable(f)
+    yield
+    for f in enabled:
+        gl.glDisable(f)
+
+
+@contextmanager
+def disabled(*gl_flags):
+    disabled = set()
+    for f in gl_flags:
+        if gl.glIsEnabled(f):
+            disabled.add(f)
+            gl.glDisable(f)
+    yield
+    for f in disabled:
+        gl.glEnable(f)
