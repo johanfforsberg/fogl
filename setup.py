0b9adfbb1dceba2f0309de0a336762559530244e
Johan Forsberg <johan@slentrian.org>
2019-12-14 14:14:00 +0100

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='fogl',
+   version='0.1',
+   description='Some OpenGL wrappers',
+   author='Johan Forsberg',
+   author_email='johan@slentrian.org',
+   packages=['fogl'],
+)
