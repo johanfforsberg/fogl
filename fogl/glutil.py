3950c8b25c2f9710876ef7854fee0ed6e9d758af
Johan Forsberg <johan@slentrian.org>
2020-12-29 13:23:06 +0100

    Add function to read the GL debug log.

--- fogl/glutil.py
+++ fogl/glutil.py
@@ -1,4 +1,4 @@
-from ctypes import byref
+from ctypes import byref, create_string_buffer
 from functools import lru_cache
 
 from pyglet import gl
@@ -23,3 +23,20 @@ GLTYPE_TO_CTYPE = {
     gl.GL_BYTE: gl.GLbyte,
     gl.GL_UNSIGNED_BYTE: gl.GLubyte
 }
+
+
+def get_error_log(maxcount=10, bufsize=1000):
+    """
+    Reads whatever is currently in the error GL log.
+    Note that this requires the GL context to have debugging enabled. In pyglet, this
+    can be done by supplying a gl.Config object with the "debug" member set to True.
+    """
+    enum_array = gl.GLenum * maxcount
+    sources = enum_array()
+    types = enum_array()
+    ids = enum_array()
+    severities = enum_array()
+    lengths = (gl.GLsizei * maxcount)()
+    log_buffer = create_string_buffer(bufsize)
+    count = gl.glGetDebugMessageLog(maxcount, bufsize, sources, types, ids, severities, lengths, log_buffer)
+    return [log_buffer[:lengths[i]].decode('ascii') for i in range(count)]
